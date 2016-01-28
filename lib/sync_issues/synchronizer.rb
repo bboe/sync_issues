b73f36270d7c9b6ad3c21e673dbef38a7782cacc
Bryce Boe <bbzbryce@gmail.com>
2016-01-27 18:01:43 -0800

    Obtain list of markdown files in provided directory or raise errors.

--- lib/sync_issues/synchronizer.rb
+++ lib/sync_issues/synchronizer.rb
@@ -1,19 +1,33 @@
+require_relative 'error'
+
 module SyncIssues
   # Synchronizer is responsible for the actual synchronization.
   class Synchronizer
     def initialize(directory, repositories)
-      @directory = check_directory(directory)
+      @files = file_list(directory)
       @repositories = check_repositories(repositories)
     end
 
     def run
-      puts "Sync #{@directory} to #{@repositories.inspect}."
+      puts "Sync #{@files.inspect} to #{@repositories.inspect}."
     end
 
     private
 
-    def check_directory(directory)
-      directory
+    def file_list(directory)
+      unless File.directory?(directory)
+        raise Error, "'#{directory}' is not a valid directory"
+      end
+
+      files = Dir.glob(File.join(directory, '**/*')).select do |entry|
+        entry.end_with?('.md') && File.file?(entry)
+      end
+
+      if files.empty?
+        raise Error, "'#{directory}' does not contain any .md files"
+      end
+
+      files
     end
 
     def check_repositories(repositories)
